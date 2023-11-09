4e296bc4c3849900d8b1b2fc95cd99d6828d2f66
Ankith Rajashekar <arajashe@terpmail.umd.edu>
2023-11-09 14:16:46 -0500

    'Testing web hook'

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import json
-
+#ayo
 with open('/etc/config.json') as config_file:
 	config = json.load(config_file)
 
