3acf137c1153275b88adcef71c8a5131a2013008
Ankith Rajashekar <arajashe@terpmail.umd.edu>
2023-11-25 17:54:32 -0500

    'Deleted Unnecessary Comments'

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "45.79.55.145", "www.arajashe.blog"]
 
