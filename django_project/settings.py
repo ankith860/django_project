1e4e6f075d8f4e9185e22b3bb214dc3d331a50b5
Ankith Rajashekar <arajashe@terpmail.umd.edu>
2023-11-24 20:43:00 -0500

    'Re-Deployment'

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,11 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config['SECRET_KEY']
-#'SECRET_DJANGO_KEY'
+# 'SECRET_DJANGO_KEY'
+
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "45.79.55.145", "www.arajashe.blog"]
 
